d49cd5c13d550d4b331d4fcb565e1677f73232f1
Denis <denis21.97@mail.ru>
2018-12-11 16:26:43 +0300

    First commit with all files

---
+++ src/sample/Main.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("authorization.fxml"));
+        primaryStage.setTitle("Поиск коллокаций");
+        primaryStage.setScene(new Scene(root, 212, 147));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
