8e2d1a67b4b0ac64aa1ce76d4c3a5d0beb82ae85
Denis <denis21.97@mail.ru>
2018-12-11 18:37:15 +0300

    GeneticAlgorithm

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("authorization.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../forms/authorization.fxml"));
         primaryStage.setTitle("Поиск коллокаций");
         primaryStage.setScene(new Scene(root, 212, 147));
         primaryStage.show();
